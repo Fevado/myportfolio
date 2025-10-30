1a5cc3c81f125ec01b84171de153c1e2751ec1d3
favoured mwange <mwangefavoured@gmail.com>
2025-10-30 12:29:28 +0300

    Changes path for project images

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,10 @@
+
 export const projects = [
     {
       title: "Pakisha — Food Donation App",
       description: "A mobile app built with Flutter & Firebase connecting food donors to recipients.",
       tech: ["Flutter", "Firebase", "Dart"],
-      image: "public/Pakishaz.png", // put image in public folder
+      image: "Pakishaz.png", // put image in public folder
       repoUrl: "https://github.com/Fevado/pakisha",
       liveUrl: "https://pakisha-app.web.app/",
     },
@@ -11,7 +12,7 @@ export const projects = [
       title: "Weatherly — Weather App",
       description: "Displays real-time weather conditions for different cities using Flutter.",
       tech: ["Flutter", "Dart", "API"],
-      image: "public/cloudy.png",
+      image: "cloudy.png",
       repoUrl: "https://github.com/Fevado/weatherly",
       liveUrl: "https://weatherly-ap.web.app/",
     },
@@ -19,7 +20,7 @@ export const projects = [
       title: "Wanderlust Ventures — Travel Website",
       description: "A travel website built with HTML, CSS & JavaScript.",
       tech: ["HTML", "CSS", "JavaScript"],
-      image: "public/bali.jpg",
+      image: "bali.jpg",
       repoUrl: "https://github.com/Fevado/wanderlust_adventures",
       liveUrl: "https://wanderlustventures.netlify.app",
     },
